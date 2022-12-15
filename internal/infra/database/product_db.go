18bc23601e28e869b223c2383fe5dae52d1e98b8
Raul França <raulmfranca@gmail.com>
2022-12-15 20:09:29 -0300

    update product_db

--- internal/infra/database/product_db.go
+++ internal/infra/database/product_db.go
@@ -19,11 +19,11 @@ func NewProduct(db *gorm.DB) *Product {
 	return &Product{DB: db}
 }
 
-func (p Product) Create(product *entity.Product) error {
+func (p *Product) Create(product *entity.Product) error {
 	return p.DB.Create(&product).Error
 }
 
-func (p Product) FindAll(page, limit int, sort string) ([]entity.Product, error) {
+func (p *Product) FindAll(page, limit int, sort string) ([]entity.Product, error) {
 	var products []entity.Product
 	var err error
 
@@ -39,8 +39,25 @@ func (p Product) FindAll(page, limit int, sort string) ([]entity.Product, error)
 	return products, err
 }
 
-func (p Product) FindByID(id string) (*entity.Product, error) {
+func (p *Product) FindByID(id string) (*entity.Product, error) {
 	var product = entity.Product{}
 	err := p.DB.Where("id = ?", id).First(&product).Error
 	return &product, err
+}
+
+func (p *Product) Update(product *entity.Product) error {
+	//var pr *entity.Product
+	_, err := p.FindByID(product.ID.String())
+	if err != nil {
+		return err
+	}
+	return p.DB.Save(product).Error
+}
+
+func (p *Product) Delete(id string) error {
+	product, err := p.FindByID(id)
+	if err != nil {
+		return err
+	}
+	return p.DB.Delete(product).Error
 }
--- internal/infra/database/product_db_test.go
+++ internal/infra/database/product_db_test.go
@@ -65,3 +65,70 @@ func TestProduct_FindAll(t *testing.T) {
 	assert.Equal(t, "Product 21", products[0].Name)
 	assert.Equal(t, "Product 23", products[2].Name)
 }
+
+func TestFindProductByID(t *testing.T) {
+
+	var db *gorm.DB
+	db, err := gorm.Open(sqlite.Open("file::memory:"), &gorm.Config{})
+	if err != nil {
+		t.Errorf("failed to connect database, error: %v", err)
+	}
+	err = db.AutoMigrate(&entity.Product{})
+	if err != nil {
+		t.Errorf("failed to migrate the schema, error: %v", err)
+	}
+
+	product, err := entity.NewProduct("Product 1", 10.00)
+	assert.NoError(t, err)
+	db.Create(product)
+	productDB := NewProduct(db)
+	product, err = productDB.FindByID(product.ID.String())
+	assert.NoError(t, err)
+	assert.Equal(t, "Product 1", product.Name)
+}
+
+func TestUpdateProduct(t *testing.T) {
+
+	var db *gorm.DB
+	db, err := gorm.Open(sqlite.Open("file::memory:"), &gorm.Config{})
+	if err != nil {
+		t.Errorf("failed to connect database, error: %v", err)
+	}
+	err = db.AutoMigrate(&entity.Product{})
+	if err != nil {
+		t.Errorf("failed to migrate the schema, error: %v", err)
+	}
+	product, err := entity.NewProduct("Product 1", 10.00)
+	assert.NoError(t, err)
+	db.Create(product)
+	productDB := NewProduct(db)
+	product.Name = "Product 2"
+	err = productDB.Update(product)
+	assert.NoError(t, err)
+	product, err = productDB.FindByID(product.ID.String())
+	assert.NoError(t, err)
+	assert.Equal(t, "Product 2", product.Name)
+}
+
+func TestDeleteProduct(t *testing.T) {
+	var db *gorm.DB
+	db, err := gorm.Open(sqlite.Open("file::memory:"), &gorm.Config{})
+	if err != nil {
+		t.Errorf("failed to connect database, error: %v", err)
+	}
+	err = db.AutoMigrate(&entity.Product{})
+	if err != nil {
+		t.Errorf("failed to migrate the schema, error: %v", err)
+	}
+
+	product, err := entity.NewProduct("Product 1", 10.00)
+	assert.NoError(t, err)
+	db.Create(product)
+	productDB := NewProduct(db)
+
+	err = productDB.Delete(product.ID.String())
+	assert.NoError(t, err)
+
+	_, err = productDB.FindByID(product.ID.String())
+	assert.Error(t, err)
+}
